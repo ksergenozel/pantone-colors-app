65bdf9f2e055c59310f4d673c29f5809cab9e91f
ksergenozel <ksergenozel@gmail.com>
2025-04-05 14:36:59 +0300

    fix: pagination

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,8 +1,8 @@
-export default function Error() {
+export default function Error({ message }: { message?: string }) {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <p className="text-lg font-semibold text-red-700">
-        An error occurred while fetching colors. Please try again later.
+        {message || "An error occurred while fetching data."}
       </p>
     </div>
   );
--- src/pages/ColorsPage.tsx
+++ src/pages/ColorsPage.tsx
@@ -1,4 +1,5 @@
-import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import { useColors } from "@/hooks/useColors";
 import Pagination from "@/components/Pagination";
 import Loading from "@/components/Loading";
@@ -6,34 +7,37 @@ import Error from "@/components/Error";
 import ColorList from "@/components/ColorList";
 
 export default function PalettePage() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const pageParam = searchParams.get("page");
-  const page = Math.max(parseInt(pageParam || "1"), 1);
+  const [page, setPage] = useState<number>(1);
+
   const { data, isLoading, isError } = useColors(page);
 
   const handlePageChange = (newPage: number) => {
-    setSearchParams({ page: newPage.toString() });
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    setPage(newPage);
   };
 
+  useEffect(() => {
+    if (!isLoading && data) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [data, isLoading]);
+
   if (isLoading) return <Loading />;
-
   if (isError) return <Error />;
+  if (!data) return <Error message="No data available" />;
 
-  if (data)
-    return (
-      <main className="flex flex-col items-center gap-8 py-12 px-8 bg-zinc-50 min-h-screen">
-        <ColorList colors={data.data} />
+  return (
+    <main className="flex flex-col items-center gap-8 py-12 px-8 bg-zinc-50 min-h-screen w-full">
+      <ColorList colors={data.data} />
 
-        <Pagination
-          page={data.page}
-          totalPages={data.totalPages}
-          hasNextPage={data.hasNextPage}
-          hasPrevPage={data.hasPrevPage}
-          nextPage={data.nextPage}
-          prevPage={data.prevPage}
-          onPageChange={handlePageChange}
-        />
-      </main>
-    );
+      <Pagination
+        page={data.page}
+        totalPages={data.totalPages}
+        hasNextPage={data.hasNextPage}
+        hasPrevPage={data.hasPrevPage}
+        nextPage={data.nextPage}
+        prevPage={data.prevPage}
+        onPageChange={handlePageChange}
+      />
+    </main>
+  );
 }
