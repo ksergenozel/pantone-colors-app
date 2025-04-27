4ba295673a7251a46bd3b1436f89b2fd47f5f75f
ksergenozel <ksergenozel@gmail.com>
2025-04-27 14:34:00 +0300

    refactor: update icons and title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,13 @@
 export default function Title({ color }: { color?: string }) {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row items-center">
+      <img
+        src="/pantone-colors-icon.png"
+        alt="PANTONE Colors Icon"
+        className="w-8 h-8 mr-3"
+      />
       <p
-        className="text-[1.6rem] text-shadow-zinc-950 font-extrabold inline font-stretch-105% relative mr-3.5"
+        className="text-[1.6rem] text-zinc-950 font-extrabold inline font-stretch-105% relative mr-3.5"
         style={{ lineHeight: "1.75rem", color: color || "#09090b" }}
       >
         PANTONE
